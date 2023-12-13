2a7c6300c308f7cf66ac3c2f2113f7ebb72cc53e
Gerardo Miranda <gerardodev25@gmail.com>
2023-12-13 17:58:31 -0400

    docs(Storybook): :memo: 009 Tarea Propiedades y controles adicionales
    009 Tarea Propiedades y controles adicionales
    
    009 Tarea Propiedades y controles adicionales

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -9,11 +9,25 @@ export interface MyLabelProps {
    * this is the tag's size
    */
   size: 'normal' | 'h1' | 'h2' | 'h3';
+  /**
+   * define if the text will be uppercase or not.
+   */
+  allCaps?: boolean;
+  /**
+   * define the color of the tag.
+   */
+  color?: 'primary' | 'secundary' | 'tertiary';
 }
 
 export const MyLabel = ({
+  allCaps = false,
+  color = 'primary',
   label = 'No Label',
   size = 'normal',
 }: MyLabelProps) => {
-  return <span className={`${size} ${size}`}>{label}</span>;
+  return (
+    <span className={`${size} text-${color}`}>
+      {allCaps ? label.toLocaleUpperCase() : label}
+    </span>
+  );
 };
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -8,6 +8,21 @@ const meta = {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  argTypes: {
+    size: { control: 'select' },
+    allCaps: {
+      control: 'boolean',
+      defaultValue: false,
+      description:
+        'this parameter define if the text will be uppercase or normal',
+    },
+    color: {
+      control: 'select',
+      type: 'string',
+      defaultValue: 'primary',
+      description: 'this property define the component color',
+    },
+  },
 } satisfies Meta<typeof MyLabel>;
 
 export default meta;
@@ -16,19 +31,32 @@ type Story = StoryObj<typeof meta>;
 
 export const Basic: Story = {
   args: {
-    label: 'hola mundo',
+    label: 'hola mundo Basic',
     size: 'normal',
+    allCaps: false,
   },
 };
+
 export const Allcaps: Story = {
   args: {
-    label: 'hola mundo',
+    label: 'hola mundo AllCAps',
     size: 'h1',
+    allCaps: true,
   },
 };
+
 export const Secundary: Story = {
   args: {
     label: 'hola mundo secudary',
     size: 'h2',
+    color: 'secundary',
+  },
+};
+
+export const Tertiary: Story = {
+  args: {
+    label: 'hola mundo Tertiary',
+    size: 'normal',
+    color: 'tertiary',
   },
 };
